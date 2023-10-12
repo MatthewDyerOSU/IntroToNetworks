8233e4092a0529a3818e2642fcc319f020cc64e8
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-10-12 10:15:25 -0700

    refactored webclient.py so all functionality is properly encapsulated into functions

--- webclient.py
+++ webclient.py
@@ -6,44 +6,54 @@ def checkNumArgs(argv):
         print("Incorrect number of arguments")
         exit(1)
 
+def parseAddressAndPort(args):
+    address = args[1]
+    if len(args) > 2:
+        port = int(args[2])
+    else:
+        port = 80
+    return address, port
+
+def buildHttpReq(path, address):
+    request = (f'GET /{path} HTTP/1.1\r\n'
+                f'Host: {address}\r\n'
+                'Connection: close\r\n'
+                '\r\n')
+    return request
+
+def decodeResponse(socket):
+    decoded = ""
+    while True:
+        data = socket.recv(4096)
+        decoded += data.decode("ISO-8859-1")
+        if len(data) == 0:
+            # Close socket when done
+            socket.close()
+            break
+    return decoded
+
 def main():
     args = sys.argv
     # Check input
     checkNumArgs(args)
 
     # Establish address and port number
-    address = args[1]
-    if len(args) > 2:
-        port = args[2]
-    else:
-        port = 80
+    address, port = parseAddressAndPort(args)
 
     # Make a new socket and connect to it
     s = socket.socket()
     s.connect((address, port))
-
-
+    
     # Build and send the HTTP request
-    req = ('GET / HTTP/1.1\r\n'
-           f'Host: {address}\r\n'
-           'Connection: close\r\n'
-           '\r\n')
+    req = buildHttpReq("", address)
     
     # Encode string to bytes
     bytes = req.encode("ISO-8859-1")
     s.sendall(bytes)
 
     # Receive the web response
-    decoded = ""
-    while True:
-        data = s.recv(4096)
-        decoded += data.decode("ISO-8859-1")
-        if len(data) == 0:
-            # Close socket when done
-            s.close()
-            break
-
-    # print(decoded)
+    decoded = decodeResponse(s)
+    print(decoded)
     
 if __name__ == "__main__":
     main()
